9d1fa049bbf339d9f83bfd240d32f924eea472b1
Charles Le Roux <ch.leroux@hotmail.com>
2017-06-12 11:49:22 +0200

    Database Declaration

--- Garage20/DataAccess/GarageContext.cs
+++ Garage20/DataAccess/GarageContext.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Data.Entity;
+using System.Data.Entity;//added to use DbContext
 
 namespace Garage20.DataAccess
 {
-    public class GarageContext:DbContext
+    public class GarageContext:DbContext //Db Context permits to use the dtatbase
     {
-        public GarageContext():base("DefaultConnection")
+        public GarageContext():base("DefaultConnection") //connection for the database?
         {
 
         }
--- Garage20/Models/ParkedVehicle.cs
+++ Garage20/Models/ParkedVehicle.cs
@@ -1,24 +1,22 @@
-﻿using System;
+﻿using Garage20.Enum;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+
 
 namespace Garage20.Models
 {
     public class ParkedVehicle
     {
-        public enum Type
-        {
-            Car,
-            Motorcycle,
-            Boat,
-            Airplane,
-        }
-
+        public int Id { get; set; }
         public string RegNr { get; set; }
         public string Color { get; set; }
         public string Brand { get; set; }
         public string Model { get; set; }
         public int WheelsAmount { get; set; }
+        public VehicleType VehicleType { get; set; }
     }
+
+    
 }
