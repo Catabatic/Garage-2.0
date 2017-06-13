6819583a56002307e6af7036b37420beb9a586c1
Shaakra <linus@tollgard.com>
2017-06-13 11:54:04 +0200

    Validations added to creating a vehicle

--- Garage20/Models/ParkedVehicle.cs
+++ Garage20/Models/ParkedVehicle.cs
@@ -8,17 +8,19 @@ using System.Web;
 
 namespace Garage20.Models
 {
-    public class ParkedVehicle
+    public class ParkedVehicle 
     {
         public int Id { get; set; }
-        [RegularExpression(@"^[A-Z]+[A-Z]+[A-Z]+[0-9]+[0-9]+[0-9]*$")]
+        [RegularExpression(@"^[a-zA-Z0-9]*$", ErrorMessage = "Only letters and numbers are allowed")]
         public string RegNr { get; set; }
-        [RegularExpression(@"^[A-Z]|[a-zA-Z''-'\s]*$")]
+        [RegularExpression(@"^[a-zA-Z]*$", ErrorMessage = "Only letters are allowed")]
         public string Color { get; set; }
-        [RegularExpression(@"^[A-Z]|[a-zA-Z''-'\s]*$")]
+        [RegularExpression(@"^[a-zA-Z]*$", ErrorMessage = "Only letters are allowed")]
         public string Brand { get; set; }
+        [RegularExpression(@"^[a-zA-Z0-9]*$", ErrorMessage = "Only letters and numbers are allowed")]
         public string Model { get; set; }
-        
+
+        [RegularExpression(@"^[0-9]*$", ErrorMessage = "Requires a valid number")]
         public int WheelsAmount { get; set; }
         public VehicleType VehicleType { get; set; }
         public DateTime? CheckInTime { get; set; }
